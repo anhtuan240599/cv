665c72db6d37d29b6bf22c2f83f4436d4dcce7c2
tuanhuynh <tuanha@younetgroup.com>
2022-06-28 00:29:19 +0700

    build my cv into github page

---
+++ src/metadata/metadata.js
@@ -0,0 +1,71 @@
+module.exports = {
+  name: "Huynh Anh Tuan",
+  title: "BackEnd Developer",
+  baseUrl: "https://tuanhuynh.github.io/cv/",
+  facts: {
+    Residence:
+      '<a href="#"><i class="fa fa-home fact-icon"></i>District 3, HCM</a>',
+    LinkedIn:
+      '<a href="https://www.linkedin.com/in/tuan-huynh-584710210/"><i class="fab fa-linkedin fact-icon"></i></a>',
+    GitHub:
+      '<a href="https://github.com/anhtuan240599"><i class="fab fa-github fact-icon"></i>tuanhuynh</a>',
+    Email:
+      '<a href="mailto:anhtuan240599@gmail.com">:anhtuan240599@gmail.com</a>',
+  },
+  skills: [
+    ["Javascript"],
+    ["Typescript"],
+    ["RabbitMQ"]
+    ["MongoDB"],
+    ["Solr"]
+    ["MySQL"],
+    ["Redis"],
+    ["Docker"],
+    ["Kubernetes"],
+    ["CI/CD"],
+    ["Airflow"],
+  ],
+  summary:
+    "I am a backend developer, focusing on backend development. My ultimate goals are learn to design large-scale systems, optimize applications and lead successful teams. Love to refresh myself every single day, always hungry to keep learning!",
+  positions: [
+    {
+      company: "Younet Media",
+      location: "HCM",
+      title: "Backend Engineer",
+      period: "Aug 2021 - Present",
+      skills: [
+        "NestJS",
+        "NodeJS",
+        "Kubernetes",
+        "Redis",
+        "RabbitMQ",
+        "Docker",
+        "Solr",
+        "Mysql"
+      ],
+      contents: `
+Backend Engineer of the best company providing social media analytics solutions (Social Media Analytics) in Vietnam.
+
+Responsibilities:
+
+* Work with microservice using NestJS 
+* Create and implement jobs build raw data to export data for business 
+* Implement jobs to crawl data from e-commerce platform 
+* Work with big data and performance coding
+
+Achievements:
+
+* Applied Airflow that helps us automatic running job every day with schedule.
+* Hosted tech talks. Topics: what is airflow and how it work?.
+* Applied Slack to monitoring log, ...
+`,
+
+
+    },
+  ],
+  softSkills: `
+  * Communication and teamwork
+  * Withstand pressure of work
+  * Adapt quickly to new knowledge
+  `
+};
