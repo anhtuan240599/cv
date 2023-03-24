5e00599b5d79530f82a025ace213c3e4fdd874d8
tuanha <tuanha@younetgroup.com>
2023-03-24 16:54:56 +0700

    update cv

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -9,7 +9,7 @@ module.exports = {
     GitHub:
       '<a href="https://github.com/anhtuan240599"><i class="fab fa-github fact-icon"></i>tuanhuynh</a>',
     Email:
-      '<a href="mailto:anhtuan240599@gmail.com">:anhtuan240599@gmail.com</a>',
+      '<a href="mailto:anhtuan240599@gmail.com">anhtuan240599@gmail.com</a>',
     GPA: '3.5/4'
   },
   skills: [
@@ -41,7 +41,8 @@ module.exports = {
         "RabbitMQ",
         "Docker",
         "Solr",
-        "Mysql"
+        "Mysql",
+        "Google Bigquery"
       ],
       contents: `
 Backend Engineer of the best company providing social media analytics solutions (Social Media Analytics) in Vietnam.
@@ -55,44 +56,13 @@ Responsibilities:
 
 Achievements:
 
-* Good completion handle and improve ELT services
+* Good completion handle and improve ELT services with more than **80 million documents** per day
 * Create and improve services to compute data and scalable
-* Applied Apache Airflow to manage sequentially running data processing services.
+* Reduced compute data time for 1 week from 48 hours to 6 hours
+* Applied Apache Airflow to manage sequentially running data processing services
 * Hosted tech talks. Topics: what is airflow and how it work?.
 * Applied Slack to monitoring services
-* Quickly handle data problems and support customer requests for data, ...
-`,
-
-
-    },
-    {
-      company: "2359 Media",
-      location: "HCM",
-      title: "Backend Engineer Intern",
-      period: "May 2021 - July 2021",
-      skills: [
-        "NodeJS",
-        "Javascript",
-        "PostgreSQL",
-        "Redis",
-        "Socket io"
-      ],
-      contents: `
-Backend Engineer Intern of Asia's leading digital consultancy and the preferred choice for cross platform engagement strategies.
-
-Responsibilities:
-
-* Learn git workflow, known and working with scrum agile
-* Create the website allows you to zip the files you want to share with others or for
-yourself
-* Deploy and apply CI/CD for website
-
-Achievements:
-
-* Works well with git workflow and agile scrum
-* Improve technical skill, coding better
-* Complete the tasks on time
-* Completion create and deploy website
+* Quickly handle data problems and support customer requests for data with more than **2 billion documents**
 `,
 
 
